4cf3191855e9b31217e6ebcd11db7355b3eba578
Chris Cascioli <chriscascioli@gmail.com>
2022-03-06 14:59:42 -0500

    Added MonoGame startup path for experimentation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
-﻿using System;
+﻿
+//#define USE_MONOGAME
+
+using System;
 using System.Windows.Forms;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 
 namespace CSharpPathTracer
 {
@@ -8,11 +16,17 @@ namespace CSharpPathTracer
 		[STAThread]
 		static void Main()
 		{
+#if USE_MONOGAME
+			// Start as a monogame application
+			using (GamePathTracer game = new GamePathTracer())
+				game.Run();
+#else
 			// Start as a windows forms app
 			Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
+#endif
 		}
 	}
 }
